2062beb3c9c5abc7cca9c2203f85663107f9d87e
VOID-CREATOR <kartheekkavuru@gmail.com>
2026-08-25 10:51:08 +0530

    Secure Django secret key configuration

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c4*vj!u7r)d07r==2919fnel%$9#))*pm(@_ai8*5oe++=tbhu'
-
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-c4*vj!u7r)d07r==2919fnel%$9#))*pm(@_ai8*5oe++=tbhu"
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
